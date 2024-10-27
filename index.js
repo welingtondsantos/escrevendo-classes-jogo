49a4f79a5c495ecd27a799c5a9e9392b615a2843
wmanobl <welingtondossantos007@gmail.com>
2024-10-27 18:24:43 -0300

    commit finalizando

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+//Usando Class
+
+class heroiAventura{
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    //Usando Metodo
+
+    atacar (){
+        let ataque;
+        //Estrutura de condição
+
+        if (this.tipo === "Ninja"){
+            ataque = "usando shuriken";
+        }
+        else if (this.tipo === "Guerreiro"){
+            ataque = "usando espada";
+        }
+        else if (this.tipo === "Monge"){
+            ataque = "usando artes marciais";
+        }
+        else if (this.tipo === "Mago"){
+            ataque = "usando magia";
+        }
+        return `O ${this.tipo} atacou ${ataque}`;
+    }
+}
+// Usando instância
+
+let heroi = new heroiAventura ("Shihiro", 25, "Mago");
+
+console.log(heroi.atacar());
